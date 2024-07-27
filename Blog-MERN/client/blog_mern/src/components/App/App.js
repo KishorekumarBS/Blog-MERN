4a55b6274acf21a38f356f633ec4f973eff9a960
KishorekumarBS <hksankaran@gmail.com>
2024-07-27 14:51:59 -0700

    First commit

---
+++ Blog-MERN/client/blog_mern/src/components/App/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image"><img src="https://spectrum.ieee.org/media-library/photo-of-a-bearded-man-in-glasses-blue-shirt-and-jacket.png?id=33321321&width=1400&height=1050&quality=85&coordinates=0%2C36%2C0%2C95
+        " alt="post image" /></div>
+        <div className='texts'>
+        <h2>The EV Transition Explained </h2>
+        <p>A deep dive into the engineering challenges of making and supporting electric vehicles at scale</p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image"><img src="https://spectrum.ieee.org/media-library/photo-of-a-bearded-man-in-glasses-blue-shirt-and-jacket.png?id=33321321&width=1400&height=1050&quality=85&coordinates=0%2C36%2C0%2C95
+        " alt="post image" /></div>
+        <div className='texts'>
+        <h2>The EV Transition Explained </h2>
+        <p>A deep dive into the engineering challenges of making and supporting electric vehicles at scale</p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image"><img src="https://spectrum.ieee.org/media-library/photo-of-a-bearded-man-in-glasses-blue-shirt-and-jacket.png?id=33321321&width=1400&height=1050&quality=85&coordinates=0%2C36%2C0%2C95
+        " alt="post image" /></div>
+        <div className='texts'>
+        <h2>The EV Transition Explained </h2>
+        <p>A deep dive into the engineering challenges of making and supporting electric vehicles at scale</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
