3baff93315b00333c6d176a7bc5e80ff855e6f57
KishorekumarBS <hksankaran@gmail.com>
2024-08-09 15:35:38 -0700

    Link to individual post content

--- Blog-MERN/client/src/components/Post/Post.js
+++ Blog-MERN/client/src/components/Post/Post.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './Post.css'
 import {formatISO9075} from 'date-fns';
+import {Link} from 'react-router-dom'
 
-export default function Post({title, summary, cover, content, createdAt, author}) {
+export default function Post({_id, title, summary, cover, content, createdAt, author}) {
   return (
     <div className="post">
-        <div className="image"><img src={'http://localhost:4000/'+cover} alt="post image" /></div>
+        <div className="image">
+          <Link to = {`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt="post image" />
+          </Link>
+          </div>
         <div className='texts'>
+        <Link to = {`/post/${_id}`}>
         <h2>{title}</h2>
+        </Link>
         <p className="info">
           <a href="" className="author">{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
