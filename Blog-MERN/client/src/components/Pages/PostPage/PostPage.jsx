4197b9f25b5071746357940f74e12af484a8f523
KishorekumarBS <hksankaran@gmail.com>
2024-08-09 20:00:02 -0700

    Post page is done

--- Blog-MERN/client/src/components/Pages/PostPage/PostPage.jsx
+++ Blog-MERN/client/src/components/Pages/PostPage/PostPage.jsx
@@ -3,9 +3,15 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import api from '../Login/api'
+import './PostPage.css'
+import { formatISO9075 } from 'date-fns';
+import { useContext } from 'react';
+import { UserContext } from '../../UserContext/UserContext';
+import {Link} from 'react-router-dom'
 
 function PostPage() {
     const [postInfo, setPostInfo] = useState();
+    const {userInfo} = useContext(UserContext)
     const {id} = useParams();
     useEffect(()=>{
         api.get(`/post/${id}`).then(response =>{setPostInfo(response.data)})
@@ -13,11 +19,23 @@ function PostPage() {
     if(!postInfo) return '';
   return (
     <div>
+        <h1>{postInfo.title}</h1>
+        <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+        <div className="author">by @{postInfo.author.username}</div>
+        {userInfo.id === postInfo.author._id && (
+          <div className='edit-row'>
+
+            <Link to={`/edit/${id}`} className="edit-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
+            Edit this Post</Link>
+          </div>
+        )}
         <div className="image">
         <img src={`http://localhost:4000/${postInfo.cover}`} alt="Post image fetched from DB" />
         </div>
-        <h1>{postInfo.title}</h1>
-        <div dangerouslySetInnerHTML={{__html:postInfo.content}}></div>
+        <div className='content'><div dangerouslySetInnerHTML={{__html:postInfo.content}}></div></div>
         
     </div>
   )
